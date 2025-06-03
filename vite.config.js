d703fc33349c08d0efa4a776958a3310237952c2
simouzel1964 <simouzel@gmail.com>
2025-06-03 09:26:38 +0300

    fix: fix robots.txt

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // pioracle/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Sitemap from 'vite-plugin-sitemap';
+import Sitemap from 'vite-plugin-sitemap'; // Assuming this is your import
 
 export default defineConfig(async () => {
   return {
@@ -9,13 +9,19 @@ export default defineConfig(async () => {
       react(),
       Sitemap({
         hostname: 'https://pioracle.online',
-        dynamicRoutes: [ 
+        dynamicRoutes: [
           '/predictions',
           '/resolved-markets',
           '/my-predictions'
         ],
-        // Exclude the Google HTML verification file PATH AS THE PLUGIN SEES IT
-        exclude: ['/google3ea9863098b060a5'] // <<< REMOVE .html FROM THE EXCLUDE PATH
+        exclude: ['/google3ea9863098b060a5'],
+        robots: [ // <<< MAKE SURE THIS IS HERE
+          {
+            userAgent: '*',
+            allow: '/',
+            // The plugin should add the Sitemap directive based on this
+          },
+        ],
       }),
     ],
   };
