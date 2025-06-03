faf7a51c25e50270b7ebcf65c48824332adc1c9b
simouzel1964 <simouzel@gmail.com>
2025-06-03 09:33:24 +0300

    Further refinement of vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,7 @@
 // pioracle/vite.config.js
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import Sitemap from 'vite-plugin-sitemap'; // Assuming this is your import
+import Sitemap from 'vite-plugin-sitemap'; // Your import might be different
 
 export default defineConfig(async () => {
   return {
@@ -15,13 +15,13 @@ export default defineConfig(async () => {
           '/my-predictions'
         ],
         exclude: ['/google3ea9863098b060a5'],
-        robots: [ // <<< MAKE SURE THIS IS HERE
-          {
-            userAgent: '*',
-            allow: '/',
-            // The plugin should add the Sitemap directive based on this
-          },
-        ],
+        robotsTxtOptions: { // This is a common way to structure it, or similar
+            enabled: false // Try to find an option to disable robots.txt generation/modification
+        }
+        // OR try:
+        // generateRobotsTxt: false,
+        // OR try:
+        // robots: false, // If the plugin supports a simple boolean
       }),
     ],
   };
